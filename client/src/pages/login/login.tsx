890da6321a97d810343a9c420953b6eef1897734
dnabours <dnabours@kr.af.mil>
2022-04-04 08:45:24 -0700

    login page

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,1 +1,37 @@
-export { }
+import './login.css'
+import axios from 'axios'
+import { FormEvent } from 'react'
+
+
+function Login() {
+
+
+  const login = async () => {
+    axios.get("/auth/login").then(res => {
+      return res.data  
+    })
+  }
+
+  const handleClick = (e:FormEvent<HTMLFormElement>) => {
+    
+  }
+  return (<div className="login">
+  <div className="loginWrapper">
+    <div className="loginLeft">
+      <h3 className="loginLogo">Social Media App</h3>
+      <span className="loginDesc">
+        Connect with friends and stuff.
+      </span>
+    </div>
+    <div className="loginRight">
+      <form className="loginBox" onSubmit={handleClick}>
+        <input placeholder="Email" type="email" required className="loginInput"/>
+        <input placeholder="Password" type="password" required minLength={8} className="loginInput"/>
+        <button className="loginButton" type="submit"> 
+          Log In
+        </button> 
+      </form>
+    </div>
+  </div>
+</div>)
+}
