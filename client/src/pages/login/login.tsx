a511df91c74b5995c6c3e56b763f3be792b3fe40
dnabours <dnabours@kr.af.mil>
2022-04-19 10:46:55 -0700

    remove training content

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -5,43 +5,9 @@ import { useAuthContext } from "../../AuthContext";
 
 function LoginPage() {
   const { setUser } = useAuthContext(); 
-  const email = useRef<HTMLInputElement>(null);
-  const password = useRef<HTMLInputElement>(null);
-  const login = async (creds: { email: string; password: string }) => { 
-  };
-  const handleClick = (e: any) => { 
-  };
   return (
-    <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">Social Media App</h3>
-          <span className="loginDesc">Connect with friends and stuff.</span>
-        </div>
-        <div className="loginRight">
-          <form className="loginBox" >
-            <input
-              placeholder="Email"
-              type="email"
-              required
-              className="loginInput"
-              ref={email}
-            />
-            <input
-              placeholder="Password"
-              type="password"
-              required
-              minLength={8}
-              className="loginInput"
-              ref={password}
-            />
-            <button onClick={handleClick} className="loginButton" type="submit">
-              Log In
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <>
+  </>
   );
 }
 
